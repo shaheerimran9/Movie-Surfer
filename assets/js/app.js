2173211342846300792481ddf18b962660337ab3
Shaheer Imran <shaheerimran@ShaheersMacBook.T-mobile.com>
2020-06-28 12:13:28 -0400

    Allow movie to be stored in session storage when clicked.

--- assets/js/app.js
+++ assets/js/app.js
@@ -10,26 +10,31 @@ function createMovie(movieData, container) {
     movie.classList.add('movie');
     // Set movie poster as background of element
     movie.style.background = `url('https://image.tmdb.org/t/p/w500${movieData.poster_path}') center center/cover`
+    // Set movie ID in session storage and navigate to movie page on click
+    movie.onclick = () => {
+        sessionStorage.setItem('movieID', movieData.id)
+        window.location = 'movie.html'
+    }
     // Append movie element to movie container
     container.appendChild(movie);
 }
 
 // Fetch popular movies - landing page
 fetch(`https://api.themoviedb.org/3/movie/popular?api_key=820d6db8746f3de6a93c6c922bf8074e&language=en-US&adult=false&page=1`)
-.then(response => response.json())
-.then(data => {
-    const movies = data.results;
-    movies.forEach(movie => {
-        createMovie(movie, mostPopularMovies);
+    .then(response => response.json())
+    .then(data => {
+        const movies = data.results;
+        movies.forEach(movie => {
+            createMovie(movie, mostPopularMovies);
+        })
     })
-})
 
 // Fetch top rated movies - landing page
 fetch(`https://api.themoviedb.org/3/movie/top_rated?api_key=820d6db8746f3de6a93c6c922bf8074e&language=en-US&adult=false&page=1`)
-.then(response => response.json())
-.then(data => {
-    const movies = data.results;
-    movies.forEach(movie => {
-        createMovie(movie, topRatedMovies);
+    .then(response => response.json())
+    .then(data => {
+        const movies = data.results;
+        movies.forEach(movie => {
+            createMovie(movie, topRatedMovies);
+        })
     })
-})
